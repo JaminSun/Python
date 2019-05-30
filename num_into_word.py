bc21853741cb2a27d04c9d6f20a5033ed5cf101b
JaminSun <b.tinson@hotmail.com>
2019-05-30 10:33:30 +0100

    git commit

--- num_into_word.py
+++ num_into_word.py
@@ -4,14 +4,14 @@ tens=["twenty","thirty","fourty","fifty","sixty","seventy","eigthy","ninety"]
 num=int(input("Enter any number:"))
 ans=""
 if num>=1000 and num<=9999:
-    ans=ones[int(num/1000)]+" thousand"
+    ans+=ones[int(num/1000)]+" thousand"
     num=num%1000
-if num>=100:
-    ans=ones[int(num/100)]+" hundred"
+if num>=100 and num<=999:
+    ans+=ones[int(num/100)]+" hundred"
     num=num%100
-if num>=20:
-    ans+=tens(int(num/100)
+if num>=20 and num<=99:
+    ans+=tens(int(num/10))
     num=num%10
-if num>=0 and num<=19:
+if num>0 and num<=19:
     ans+=ones(num)
 print(ans)
